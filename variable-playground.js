f20e471962f7db6f3cbcddec3245cc7b664aab41
Saumya Suhagiya <saumya.patel2001@gmail.com>
2016-05-05 09:34:06 +0530

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+// 	name: 'Saumya',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 	name: 'Saumya',
+// 	// 	age: 26
+// 	// };
+// 	obj.age = 26;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array example
+
+var grades = [15,35];
+
+function addGrades(grades) {
+	grades.push(55);
+	debugger;
+	//grades = [12, 33, 99];
+}
+
+addGrades(grades);
+console.log(grades);
+
