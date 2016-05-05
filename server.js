02cd6940ff79cf17430a91673db74954eff5cceb
Saumya Suhagiya <saumya.patel2001@gmail.com>
2016-05-05 08:41:42 +0530

    Add PUT todos

--- server.js
+++ server.js
@@ -51,7 +51,6 @@ app.post('/todos', function (req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-
 	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (matchedTodo) {
@@ -62,6 +61,34 @@ app.delete('/todos/:id', function (req, res) {
 	} else {
 		res.status(404).json({"error": "no todo found with that id"});
 	}
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	matchedTodo = _.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+
 });
 
 app.listen(PORT, function () {
