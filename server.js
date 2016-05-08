058710d46a61527658a6dce372493d77f16b7a16
Saumya Suhagiya <saumya.patel2001@gmail.com>
2016-05-08 13:54:28 +0530

    Convert GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -40,18 +40,20 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(todoId).then(function (todo) {
+		if(!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function (e) {
+		res.status(500).send();
+	});
 });
 
 // POST /todos
 app.post('/todos', function (req, res) {
-
 	var body = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(body).then(function (todo) {
@@ -59,21 +61,6 @@ app.post('/todos', function (req, res) {
 	}, function (e) {
 		res.status(400).send();
 	});
-	//create on db.todo
-	// respond with 200 and todo
-	// e res.status(400).json(e);
-
-	// if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).send();
-	// }
-
-	// body.description = body.description.trim();
-
-	// body.id = todoNextId++;
-
-	// todos.push(body);
-
-	// res.json(body);
 });
 
 // DELETE /todos/:id
